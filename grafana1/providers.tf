f126b6659d7eab966af6fe287e1b678aca2e80ec
Muhamad Hisyam <syam9294@gmail.com>
2026-08-23 18:21:46 +0800

    feat: complete Grafana monitoring and alerting lab

---
+++ grafana1/providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.6"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.9"
+    }
+  }
+
+  backend "s3" {
+    bucket = "syam9-terraform-state-026486698952"
+    key    = "grafana/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_caller_identity" "my_account" {}
